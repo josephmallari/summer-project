16db325336113947df946e24c1d3c767de54b0c4
Joseph Mallari <joseph.mallari48@gmail.com>
2016-06-12 11:01:43 -0400

    test ig call

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,9 +3,16 @@
 	angular
 		.module('app', [])
 		.controller('myCtrl', myCtrl)
+		.factory('ig', [function(){
+			$http.get("https://api.instagram.com/v1/media/popular?access_token=2244597.277addd.6aa12926640d452db376192f209f5154")
+		}])
 
 	function myCtrl() {
 		this.holla = 'holla'
 	}
+
+
+
+
 
 })();
