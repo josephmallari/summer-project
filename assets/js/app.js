876da34e955b1b094c74eb6167e05e5338dd2a95
Joseph Mallari <joseph.mallari48@gmail.com>
2016-06-13 10:53:13 -0400

    more ig api testing

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,17 +2,22 @@
 //
 	angular
 		.module('app', [])
-		.controller('myCtrl', myCtrl)
-		.factory('ig', [function(){
-			$http.get("https://api.instagram.com/v1/media/popular?access_token=2244597.277addd.6aa12926640d452db376192f209f5154")
-		}])
+		.controller('myCtrl', function($scope, $http){
+			function fetch() {
+				$http.jsonp("https://api.instagram.com/v1/tags/nofilter/media/recent?access_token=2244597.277addd.6aa12926640d452db376192f209f5154")
+					.success(function(response){
+						console.log(response);
+					});
+				}
 
-	function myCtrl() {
-		this.holla = 'holla'
-	}
+			fetch();
+		});
+		// .factory('ig', [function(){
+		// 	$http.get("https://api.instagram.com/v1/media/popular?access_token=2244597.277addd.6aa12926640d452db376192f209f5154")
+		// }])
 
-
-
-
+	// function myCtrl() {
+	// 	this.holla = 'holla'
+	// }
 
 })();
